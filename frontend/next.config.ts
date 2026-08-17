a545ed26cff868607ed0a8bccdba8e314aa32902
Deepanshu Shrama <deepanshusharma1425@gmail.com>
2026-08-17 09:50:54 +0530

    feat: ready for Amplify deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "http", hostname: "localhost", port: "8080", pathname: "/uploads/**" },
-      { protocol: "http", hostname: "10.39.70.232", port: "8080", pathname: "/uploads/**" }
+      { protocol: "http", hostname: "10.39.70.232", port: "8080", pathname: "/uploads/**" },
+      { protocol: "http", hostname: "civicmitra-env.eba-ppjxugtc.ap-south-1.elasticbeanstalk.com", pathname: "/**" }
     ]
   }
 };
